3ad6fd636e00e342514e6ff345f902f61ab2111d
Alexandr Burtsev <KapitanZerg@gmail.com>
2023-04-02 15:34:49 +0300

    Create new project

---
+++ WorldOfReviewers/WorldOfReviewers/WorldOfReviewersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldOfReviewersApp.swift
+//  WorldOfReviewers
+//
+//  Created by Alexandr Burtsev on 02.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldOfReviewersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
